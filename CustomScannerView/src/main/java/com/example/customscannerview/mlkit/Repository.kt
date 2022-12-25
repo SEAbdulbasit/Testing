db57c64cc62f57f4fd0997b2f5da3e202a076340
Abdul Basit <abdulbasit5361234@gmail.com>
2022-12-25 10:23:47 +0500

    Ocr Request moved to Repository

--- CustomScannerView/src/main/java/com/example/customscannerview/mlkit/Repository.kt
+++ CustomScannerView/src/main/java/com/example/customscannerview/mlkit/Repository.kt
@@ -3,10 +3,13 @@ package com.example.customscannerview.mlkit
 import com.example.customscannerview.mlkit.modelclasses.OCRResponseDemo
 import com.example.customscannerview.mlkit.modelclasses.OCRResponseParent
 import com.example.customscannerview.mlkit.modelclasses.OcrResponse
+import com.example.customscannerview.mlkit.modelclasses.ocr_request.BarcodeX
+import com.example.customscannerview.mlkit.modelclasses.ocr_request.FrameX
 import com.example.customscannerview.mlkit.modelclasses.ocr_request.OCRQARequest
 import com.example.customscannerview.mlkit.modelclasses.ocr_request.OCRRequestParent
 import com.example.customscannerview.mlkit.modelclasses.ocr_request.OcrRequest
 import com.example.customscannerview.mlkit.service.OcrApiService
+import com.google.mlkit.vision.barcode.common.Barcode
 
 class Repository(private val apiService: OcrApiService) {
 
@@ -23,5 +26,29 @@ class Repository(private val apiService: OcrApiService) {
 
     private suspend fun analyseOCRQAAsync(ocrRequest: OCRQARequest): OcrResponse {
         return apiService.analyseOCRQAAsync(ocrRequest)
+    }
+
+    fun getOCRRequest(
+        barcodesList: List<Barcode>, baseImage: String, isQAVariant: Boolean
+    ): OCRRequestParent {
+        return if (isQAVariant) {
+            OCRQARequest(
+                barcode = BarcodeX(listOf(barcodesList.map { barcode ->
+                    FrameX(
+                        barcode.displayValue, barcode.format.toString()
+                    )
+                }.toList())),
+                callType = "extract",
+                extractTime = "2022-08-29T05:58:28.902Z",
+                image = baseImage,
+                orgUuid = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                platform = "API"
+            )
+
+        } else {
+            OcrRequest(image_url = "data:image/jpeg;base64,$baseImage",
+                type = "shipping_label",
+                barcode_values = barcodesList.map { it.displayValue!! })
+        }
     }
 }
